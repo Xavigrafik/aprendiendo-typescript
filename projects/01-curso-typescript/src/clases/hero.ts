4d7c780ff4e4b9f1455effcd9c4dc08eb6b87bfe
xavi <xavigrafik@gmail.com>
2025-11-13 13:19:02 +0100

    hero/powers

--- projects/01-curso-typescript/src/clases/hero.ts
+++ projects/01-curso-typescript/src/clases/hero.ts
@@ -1,10 +1,14 @@
+import powers from '../data/powers'
+
 export class Hero { 
     constructor(
         public name: string,
-        public powerId: number,
+        public id: number,
         public age:number
     ) { }
-    
 
+    get power():string { 
+        return powers.find( power => power.id === this.id )?.desc || 'Not found';
+    }
 
 }
--- projects/01-curso-typescript/src/index.ts
+++ projects/01-curso-typescript/src/index.ts
@@ -1,8 +1,12 @@
 import { Hero } from "./clases/hero";
 
 
-const ironman = new Hero("Iron Man", 10, 55);
-
+const ironman = new Hero("Iron Man", 1, 55);
 console.log(ironman);
+console.log(ironman.power);
+
+const batman = new Hero("Batman", 2, 33);
+console.log(batman);
+console.log(batman.power);
 
 
